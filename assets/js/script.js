3c15f22a854676b67b72c3caff31c8cde4e5b676
Criste Phillips <criste.phillips@yahoo.com>
2021-12-09 18:25:28 -0500

    task id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
+var pageContentEl = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
@@ -85,4 +86,45 @@ var createTaskEl = function(taskDataObj) {
   taskIdCounter++;
 };
 
-formEl.addEventListener("submit", taskFormHandler)}
+formEl.addEventListener("submit", taskFormHandler);
+// other logic
+
+var taskButtonHandler = function(event) {
+  // get target element from event
+  var targetEl = event.target;
+
+  // edit button was clicked
+  if (targetEl.matches(".edit-btn")) {
+    var taskId = targetEl.getAttribute("data-task-id");
+    editTask(taskId);
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
+  } 
+  // delete button was clicked
+  else if (targetEl.matches(".delete-btn")) {
+    var taskId = targetEl.getAttribute("data-task-id");
+    deleteTask(taskId);
+  }
+};
+      var deleteTask = function(taskId) {
+        var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+        taskSelected.remove();
+      
+        var editTask = function(taskId) {
+          console.log("editing task #" + taskId);
+        
+          // get task list item element
+          var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+          // get content from task name and type
+          var taskName = taskSelected.querySelector("h3.task-name").textContent;
+          console.log(taskName);
+
+          var taskType = taskSelected.querySelector("span.task-type").textContent;
+          document.querySelector("input[name='task-name']").value = taskName;
+          document.querySelector("select[name='task-type']").value = taskType;
+        };
+      };
+    };
+
+pageContentEl.addEventListener.Listener("click", taskButtonHandler);
